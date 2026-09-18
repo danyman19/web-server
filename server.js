bee379cd7ed17f3e7537e9bf1d86f9610b2e4d36
Daniel Qi <danielqi@umass.edu>
2026-09-18 16:32:00 +0000

    pre lab 2 commit

--- server.js
+++ server.js
@@ -1,13 +1,62 @@
 import express from "express";
-
 import apiRouter from "./routes/api.js";
+import { join } from "path";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello!!!!!");
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use(express.static("public"));
+// app.get("/", (req, res) => {
+//   res.sendFile(join(import.meta.dirname, "public", "index.html"));
+// });
+
+// app.get("/entries", (req, res) => {
+//   const entries = [
+//     { title: "First note", body: "one" },
+//     { title: "Second note", body: "two" },
+//     { title: "Third note", body: "three" },
+//   ];
+//   res.render("entries", { entries: entries });
+// });
+
+app.get("/events", (req, res) => {
+  const events = [
+    // { title: "Birthday Party" },
+    // { title: "Coding Class" },
+    // { title: "Dinner" },
+  ];
+
+  res.render("events", { events });
 });
+
+app.get("/entries", (req, res) => {
+  const entries = [
+    { title: "First note" },
+    { title: "Second note" },
+    { title: "note 3" },
+  ];
+  const inner =
+    "<ul>" + entries.map((e) => `<li>${e.title}</li>`).join("") + "</ul>";
+  res.render("layout", { title: "Entries", body: inner, entries });
+});
+app.get("/entries/:id", (req, res) => {
+  const entries = [
+    { title: "First note", body: "one" },
+    { title: "Second note", body: "two" },
+    { title: "Third note", body: "three" },
+  ];
+  const id = req.params.id;
+  if (id < 0 || id >= entries.length) {
+    res.status(404).send("Invalid ID");
+    return;
+  }
+  const inner = entries[id].title;
+  res.render("layout", { title: "Entry #" + id, body: inner });
+});
+
 app.get("/hello", (req, res) => {
   res.send("I am learning how to develop a website :D");
 });
